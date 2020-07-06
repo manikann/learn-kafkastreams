a5d7cf9df52a3e125247756babeb986ceb5b2727
Manikandan Natarajan <manikann@gmail.com>
2020-07-06 08:56:28 +0800

    Add test-containers

--- src/test/java/io/nataman/learn/kafkastreams/TestContainersTest.java
+++ src/test/java/io/nataman/learn/kafkastreams/TestContainersTest.java
@@ -1,12 +1,16 @@
 package io.nataman.learn.kafkastreams;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("testcontainers")
-public class TestContainersTest {
+@Log4j2
+@Import({TestConfiguration.class})
+class TestContainersTest {
 
   @Test
   void contextLoaded() {}
