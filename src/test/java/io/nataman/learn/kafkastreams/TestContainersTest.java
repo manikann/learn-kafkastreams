b7e9971010b716cebcd45779d9a7eb628c17e7cb
Manikandan Natarajan <manikann@gmail.com>
2020-07-06 08:41:43 +0800

    Add test-containers

---
+++ src/test/java/io/nataman/learn/kafkastreams/TestContainersTest.java
@@ -0,0 +1,13 @@
+package io.nataman.learn.kafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("testcontainers")
+public class TestContainersTest {
+
+  @Test
+  void contextLoaded() {}
+}
